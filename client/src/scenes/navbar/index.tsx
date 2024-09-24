4e8fe8be4c2ce28bfd142ba9aba3978e27712d8e
Jay Rathod <rathodjay4397@gmail.com>
2024-09-24 06:37:05 +0530

    project setup with Router

---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Box, Typography, useTheme } from "@mui/material";
+import { FlexBetween } from "../../components/FlexBetween";
+import PixIcon from "@mui/icons-material/Pix";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+type Props = unknown;
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* Left Side  */}
+      <FlexBetween gap={".75rem"}>
+        <PixIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize={"16px"}>
+          JayFinance
+        </Typography>
+      </FlexBetween>
+
+      {/* Right Side */}
+      <FlexBetween gap="2rem">
+        <Box
+          sx={{
+            "&:hover": {
+              color: palette.primary[100],
+              borderBottom: "1px white solid",
+            },
+          }}
+        >
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            Dashboard
+          </Link>
+        </Box>
+
+        <Box
+          sx={{
+            "&:hover": {
+              color: palette.primary[100],
+              borderBottom: "1px white dashed",
+            },
+          }}
+        >
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            Predictions
+          </Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
